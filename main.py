ae7f5797b082aa654a93f876b8a19993eb7c8d1a
Winbert Zhang <winbertzhang@gmail.com>
2022-07-07 14:44:43 -0700

    Add num pages parsing
    Looking through alt tag to find number of pages

--- main.py
+++ main.py
@@ -19,12 +19,19 @@ driver.implicitly_wait(5)
 urlList = set()
 
 elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3'][1]/img[@class='_2zZ8u']")
-elt.get_attribute("alt")
+altTag = elt.get_attribute("alt")
 
-for pageNum in range(1, 2):
+try:
+    numPages = int(altTag[::-1][5:altTag[::-1].find(" ", 6)])
+except:
+    print('Number of Pages not found')
+
+print("DEBUG LOG (numPages): " + str(numPages))
+
+for pageNum in range(1, numPages+1):
     elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3']["+str(pageNum)+"]/img[@class='_2zZ8u']")
     time.sleep(1)
-    print(elt.get_attribute("src"))
+    # print(elt.get_attribute("src"))
     urlList.add(elt.get_attribute("src"))
 
-print(urlList)
+print("DEBUG LOG (urlList): " + urlList)
