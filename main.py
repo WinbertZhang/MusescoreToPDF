5298b76115838104201699cf5eb5986edd4fc756
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-07-07 13:47:48 -0700

    get multiple urls

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from selenium import webdriver
 from fpdf import FPDF
+import time
 
-loop = True
-while(loop):
-    print("Enter a valid muscore url")
+while(True):
+    print("Enter a valid musescore url")
     url = input()
-
     if not (url.startswith('https://musescore.com') or url.startswith('musescore.com')):
         print('Invalid url')
     else:
-        loop = False
+        break
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 driver.get(url)
-driver.maximize_window()
+driver.implicitly_wait(5)
 
-elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3'][1]/img[@class='_2zZ8u']");
-print(elt.get_attribute("src"))
+urlList = set()
+
+for pageNum in range(1, 10):
+    elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3']["+str(pageNum)+"]/img[@class='_2zZ8u']");
+    time.sleep(1)
+    print(elt.get_attribute("src"))
+    urlList.add(elt.get_attribute("src"))
+
+print(urlList)
