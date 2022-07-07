01f2bec90abc4c1b5abd86653ba8bb75f7c238f8
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-07-07 13:29:51 -0700

    get url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from fpdf import FPDF
 
 loop = True
 while(loop):
@@ -15,3 +16,6 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 driver.maximize_window()
+
+elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3'][1]/img[@class='_2zZ8u']");
+print(elt.get_attribute("src"))
