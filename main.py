626b2b7e00399d08d5a0772f82cf7fbda8154253
cyin100 <46613498+cyin100@users.noreply.github.com>
2022-07-07 15:11:15 -0700

    functional w/o scrolling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image
 import urllib.request
 import time
 import warnings
+import os
 
 while(True):
     print("Enter a valid musescore url")
@@ -20,34 +21,28 @@ driver.get(url)
 driver.implicitly_wait(5)
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
-urlList = set()
-
 elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3'][1]/img[@class='_2zZ8u']")
 altTag = elt.get_attribute("alt")
 
 try:
-    numPages = int(altTag[::-1][5:altTag[::-1].find(" ", 6)])
+    numPages = int(altTag[::-1][5:altTag[::-1].find(" ", 6)][::-1])
 except:
     print('Number of Pages not found')
 
 print("DEBUG LOG (numPages): " + str(numPages))
 
+images = []
+
 for pageNum in range(1, numPages+1):
     elt = driver.find_element_by_xpath("/html/body[@class='js desktop js-cookie-compliance theme-react summer-sale']/div[@class='js-page react-container']/div[@class='_5tn-M']/section[@class='HJV7K']/main[@class='_1sdLO _1ss1d']/div[@class='_2G3ri']/div[@class='_3UHUA']/div[@class='_36dkj']/div[@id='jmuse-scroller-component']/div[@class='vAVs3']["+str(pageNum)+"]/img[@class='_2zZ8u']")
     time.sleep(1)
     print("DEBUG LOG (current page):" + elt.get_attribute("src"))
-    urlList.add(elt.get_attribute("src"))
+    url = elt.get_attribute("src")
+    image = urllib.request.urlretrieve(url, str(pageNum)+".png")
+    images.append(Image.open(str(pageNum)+".png"))
 
-print("DEBUG LOG (urlList): ")
-print(urlList)
-
-images = []
-count = 1
-
-for url in urlList:
-    image = urllib.request.urlretrieve(url, str(count)+"png")
-    images.append(Image.open(str(count)+"png"))
-    count += 1
-
-pdf_path = "C:\\Users\\conne\\Documents\\GitHub\\MusescoreToPDF\\test.pdf"
+pdf_path = "C:\\Users\\conne\\Documents\\GitHub\\MusescoreToPDF\\sheetmusic.pdf"
 images[0].save(pdf_path, "PDF" ,resolution=100.0, save_all=True, append_images=images[1:])
+
+for pageNum in range(1, numPages+1):
+    os.remove(str(pageNum)+".png")
